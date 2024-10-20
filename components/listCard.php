be7552018934513613289c3f93b259cd45b2b412
PerecerDev <perecer.dev@gmail.com>
2024-10-20 16:19:51 +0200

    styling cards

--- components/listCard.php
+++ components/listCard.php
@@ -1,15 +1,19 @@
 <?php function listCard() { ob_start();
-$query = get_current_query();
 
-if ($query->have_posts()) {
-    echo '<ul>';
-    while ($query->have_posts()) {
-        $query->the_post();
-        ele('eventCard');
-    }
-    echo '</ul>';
-}
+    $query = get_current_query();
 
-return ob_get_clean();
+    if ($query->have_posts()) { ?>
 
-} ?>
+<div class="list-card">
+    <ul>
+        <?php while ($query->have_posts()) { $query->the_post(); ?>
+
+        <?php ele('eventCard');
+        } ?>
+
+    </ul>
+</div>
+
+<?php }
+
+return ob_get_clean(); } ?>
