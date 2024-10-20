bef203f01c021b26d5b5feb77c0bc640a5d71f3a
PerecerDev <perecer.dev@gmail.com>
2024-10-20 23:18:12 +0200

    naming blocks correctly

--- build/event-list/index.asset.php
+++ build/event-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '48a9ed034e2942bb2590');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'aee47bf15f6722989b74');
--- build/main-events/index.asset.php
+++ build/main-events/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '0e69ecd09d218ddfa845');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'd68f5d16966900cd20ce');
